96f43094e3caf9280c89118230ae2d133cbbf329
PratapTechSolutions <kpreddy2k22@gmail.com>
2024-10-19 21:44:38 -0500

    entry point for my app index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// AWS Amplify Config
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';  // Amplify configuration file
+Amplify.configure(awsconfig);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
